32bd7c3cf75d8d81fb8b8c824e6e6105d5739dc0
rodrigocam <rodrigo.redcode@gmail.com>
2018-06-26 22:37:48 -0300

    Fix missing quotes

--- {{cookiecutter.project_name}}/config/settings.py
+++ {{cookiecutter.project_name}}/config/settings.py
@@ -6,7 +6,7 @@ import environ
 import os
 
 ROOT_DIR = environ.Path(__file__) - 2
-APPS_DIR = ROOT_DIR.path({{cookiecutter.project_name}})
+APPS_DIR = ROOT_DIR.path('{{cookiecutter.project_name}}')
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
